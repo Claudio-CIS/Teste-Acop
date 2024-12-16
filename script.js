39bf7cc1b60ebef2577cfd47bf91dcf5c49f1def
Claudio-CIS <acop.pinheiros@gmail.com>
2024-12-16 00:51:48 -0300

    Atualizacao 01.1.2

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
+//For it //pegar botao //abre pop up
 const openButtons = document.querySelectorAll('.open-modal');
-
+//acionamento do botao
 openButtons.forEach(button =>{
     button.addEventListener('click', () =>{
+        //endereco do botao
         const modalId = button.getAttribute('data-modal');
-        const modal = document.getElementById(modalId)
-
+        const modal = document.getElementById(modalId)       
+        //mostrar Pop Up
         modal.showModal();
     });
 });
-
+//fecha pop up
 const closeButtons = document.querySelectorAll('.close-modal');
-
+//acionamento do botao
 closeButtons.forEach(button =>{
     button.addEventListener('click', () =>{
+        //pega o Id do botao 
         const modalId = button.getAttribute('data-modal');
         const modal = document.getElementById(modalId);
-
+        //oculta pop up
         modal.close();
     });
 });
+
